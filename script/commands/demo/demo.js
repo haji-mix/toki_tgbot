738e90a702417aa5c98720f83ad7395e4539c4e6
haji-mix <hajime20041029@gmail.com>
2025-06-07 23:48:53 +0000

    update

--- script/commands/demo/demo.js
+++ script/commands/demo/demo.js
@@ -1,4 +1,4 @@
-const { createChat } = require('../utils/chat');
+
 
 module.exports = {
   config: {
@@ -8,15 +8,12 @@ module.exports = {
     vip: false, // No VIP access required
     description: 'Sends a message and responds with "hi" if a user replies to it with "hello".',
     execute: async ({ bot, chat, msg, args, chatId, userId, config, addAnswerCallback }) => {
-      const sentMessage = await chat.reply({
-        body: 'Reply to this message with "hello"!'
-      });
+      const sentMessage = await chat.reply('Reply to this message with "hello"!');
 
       const replyCallbackId = sentMessage.message_id;
-      global.replyCallbacks.set(replyCallbackId, async (replyMsg) => {
-        const replyChat = await createChat(bot, replyMsg);
-        if (replyMsg.text && replyMsg.text.toLowerCase() === 'hello') {
-          await replyChat.reply('hi');
+      global.replyCallbacks.set(replyCallbackId, async () => {
+        if (msg.text && msg.text.toLowerCase() === 'hello') {
+          chat.reply('hi');
         }
       });
     }
