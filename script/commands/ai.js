040c3289810f32bdd03c415aefd6b150f6e8faaa
haji-mix <hajime20041029@gmail.com>
2025-06-09 22:00:43 +0000

    update

--- script/commands/ai.js
+++ script/commands/ai.js
@@ -3,7 +3,6 @@ module.exports = {
     name: 'ai',
     description: 'Ai GPT4o Powered by Openai.',
     execute: async ({ chat, args, userId }) => {
-        
       const prompt = args.join(" ");
     
       if (!prompt) return chat.reply("Please provide your prompt!");
@@ -11,34 +10,39 @@ module.exports = {
       const fetching = await chat.reply("Generating response...");
       
       try {
-      
-      let response = await gpt4o(prompt, userId)
-      const sentMessage = await chat.reply(response);
-      chat.delete(fetching);
+        let response = await gpt4o(prompt, userId);
+        const sentMessage = await chat.reply(response);
+        chat.delete(fetching);
 
-      global.replyCallbacks.set(sentMessage.message_id, async (replyMsg) => {
-        if (replyMsg.text) {
-           response = await gpt4o(replyMsg.text, userId);
-           response = await chat.reply(response);
-           global.replyCallbacks.set(response.message_id);
-        }
-      });
+        global.replyCallbacks.set(sentMessage.message_id, async (replyMsg) => {
+          if (replyMsg.text) {
+            const newResponse = await gpt4o(replyMsg.text, userId);
+            const newSentMessage = await chat.reply(newResponse);
+            
+            global.replyCallbacks.set(newSentMessage.message_id, async (newReplyMsg) => {
+              if (newReplyMsg.text) {
+                const anotherResponse = await gpt4o(newReplyMsg.text, userId);
+                await chat.reply(anotherResponse);
+              }
+            });
+          }
+        });
       } catch (error) {
-          chat.delete(fetching);
-          chat.reply(error.message);
+        chat.delete(fetching);
+        chat.reply(error.message);
       }
     }
   }
 };
 
 async function gpt4o(ask, uid) {
-    try {
-const axios = require("axios");
-const msg = await axios.post(global.api.hajime + "/api/gpt4o", {
-          ask, uid
-      });
-        return msg.data.answer;
-      } catch (error) {
-          throw error;
-      }
+  try {
+    const axios = require("axios");
+    const msg = await axios.post(global.api.hajime + "/api/gpt4o", {
+      ask, uid
+    });
+    return msg.data.answer;
+  } catch (error) {
+    throw error;
   }
+}
