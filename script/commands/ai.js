9306ce63f2ff0f8f7c5e9cd417ac2cfa9a467612
haji-mix <hajime20041029@gmail.com>
2025-06-09 21:35:52 +0000

    update

---
+++ script/commands/ai.js
@@ -0,0 +1,40 @@
+module.exports = {
+  config: {
+    name: 'ai',
+    description: 'Ai GPT4o Powered by Openai.',
+    execute: async ({ chat, args, userId }) => {
+        
+      const prompt = args.join(" ");
+    
+      if (!prompt) return chat.reply("Please provide your prompt!");
+      
+      try {
+      
+      let response = await gpt4o(prompt, userId)
+      const sentMessage = await chat.reply(response);
+
+      global.replyCallbacks.set(sentMessage.message_id, async (replyMsg) => {
+        if (replyMsg.text) {
+           response = await gpt4o(replyMsg.text, userId);
+           chat.reply(response);
+        }
+      });
+      } catch (error) {
+          chat.reply(error.message);
+      }
+    }
+  }
+};
+
+async function gpt4o(prompt, uid) {
+    try {
+const axios = require("axios");
+const msg = await axios.post(global.api.hajime + "/api/gpt4o", {
+          ask: prompt,
+          uid: userId
+      });
+        return msg.data.answer;
+      } catch (error) {
+          throw error;
+      }
+  }
